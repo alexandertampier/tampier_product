950ad6687711b21d81941387245916d1e6a16f01
flywithwings <alexander.tampier@gmx.at>
2016-03-26 23:34:52 +0100

    Created company.java

---
+++ src/at/spengergasse/product/domain/Company.java
@@ -0,0 +1,11 @@
+package at.spengergasse.product.domain;
+
+import java.util.*;
+
+public class Company {
+	private List<Product> products;
+
+	public Company() {
+		this.product = new ArrayList<Product>();
+	}		
+}

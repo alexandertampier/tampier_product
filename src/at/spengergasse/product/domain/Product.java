e32e6bc72e2e43751a06f71abd522f7f8bd8a118
flywithwings <alexander.tampier@gmx.at>
2016-03-26 23:05:53 +0100

    First Version + added .classpath to gitignore!

---
+++ src/at/spengergasse/product/domain/Product.java
@@ -0,0 +1,19 @@
+package at.spengergasse.product.domain;
+
+public class Product {
+	private String name;
+
+	public Product() {
+		this.name = "Coca cola";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+		
+}

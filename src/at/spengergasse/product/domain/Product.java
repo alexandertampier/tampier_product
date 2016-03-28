fa4836166257d2af1f4c04e1d242155e6ee351d6
flywithwings <alexander.tampier@gmx.at>
2016-03-28 22:22:04 +0200

    Added onStock in Product.java

--- src/at/spengergasse/product/domain/Product.java
+++ src/at/spengergasse/product/domain/Product.java
@@ -2,9 +2,11 @@ package at.spengergasse.product.domain;
 
 public class Product {
 	private String name;
+	private boolean onStock;
 
 	public Product() {
 		this.name = "Coca cola";
+		this.onStock = true;
 	}
 
 	public String getName() {
@@ -13,6 +15,14 @@ public class Product {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public boolean isOnStock() {
+		return onStock;
+	}
+
+	public void setOnStock(boolean onStock) {
+		this.onStock = onStock;
 	}
 	
 		
